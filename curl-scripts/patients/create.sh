9da194dc8c9bedde30ae3998c3a0d23a343cc91d
fionabeatricewong <fionabeatricewong@gmail.com>
2020-09-16 14:58:15 -0400

    Removed comma at the end of email and changed _name to Name for first, middle, and last

--- curl-scripts/patients/create.sh
+++ curl-scripts/patients/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# run TOKEN="9ca950b0f676a0f326fed2e7aec66773" FIRSTNAME="Jane" LASTNAME="Doe" DOB="1990-01-01" GENDER="female" PHONE="1234567891" EMAIL="jane@doe.com" sh curl-scripts/patients/create.sh
+# run: TOKEN="9ca950b0f676a0f326fed2e7aec66773" FIRSTNAME="Jane" LASTNAME="Doe" DOB="1990-01-01" GENDER="female" PHONE="1234567891" EMAIL="jane@doe.com" sh curl-scripts/patients/create.sh
 
 API="http://localhost:4741"
 URL_PATH="/patients"
@@ -11,13 +11,13 @@ curl "${API}${URL_PATH}" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
     "patient": {
-      "first_name": "'"${FIRSTNAME}"'",
-      "middle_name": "'"${MIDDLENAME}"'",
-      "last_name": "'"${LASTNAME}"'",
+      "firstName": "'"${FIRSTNAME}"'",
+      "middleName": "'"${MIDDLENAME}"'",
+      "lastName": "'"${LASTNAME}"'",
       "dob": "'"${DOB}"'",
       "gender": "'"${GENDER}"'",
       "phone": "'"${PHONE}"'",
-      "email": "'"${EMAIL}"'",
+      "email": "'"${EMAIL}"'"
     }
   }'
 
