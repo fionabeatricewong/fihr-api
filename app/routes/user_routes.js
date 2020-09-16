d18e676c53ac6c68dd0536098f65f28207228f5f
fionabeatricewong <fionabeatricewong@gmail.com>
2020-09-16 18:28:12 -0400

    Changed 'full_name' to 'fullName' in sign up route

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -44,7 +44,7 @@ router.post('/sign-up', (req, res, next) => {
     .then(hash => {
       // return necessary params to create a user
       return {
-        fullName: req.body.credentials.full_name,
+        fullName: req.body.credentials.fullName,
         specialty: req.body.credentials.specialty,
         email: req.body.credentials.email,
         hashedPassword: hash
